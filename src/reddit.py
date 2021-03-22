a4d21b7168ba0418c1759576e54e8e498b16343d
Matthew Wong <matthew@adveca.com>
2021-03-21 21:34:09 -0700

    progress update

--- src/reddit.py
+++ src/reddit.py
@@ -1,7 +1,6 @@
 """Reddit class for API interaction. Basically bootleg PRAW."""
 import requests
 
-
 def handle_response(response):
     """Handles a web response and returns the data if response
        is good. Input is requests.Response object.
@@ -13,6 +12,10 @@ def handle_response(response):
     response.raise_for_status()
     # something bad happened
     return None
+
+def auth(auth_url, client_details, login_details, user):
+    """Maybe moving auth outside of the class."""
+
 
 class Reddit:
     """A class to represent a Reddit session.
@@ -26,8 +29,7 @@ class Reddit:
                                   user_agent)
 
     """
-
-    def __init__(self, client_details, auth_details, user_agent):
+    def __init__(self, client_details, login_details, user_agent):
         """Initialize Reddit session.
 
         client_details: a tuple of the form (client_id, client_secret)
@@ -35,31 +37,15 @@ class Reddit:
         user_agent: a string containing the user agent
         """
         # unpack arguments with tuple comprehension
+        # pylint: disable=too-many-instance-attributes
         self._client_id, self._client_secret = client_details
-        self._username, self._password = auth_details
+        self._username, self._password = login_details
         self._user_agent = user_agent
 
         self._url = "https://www.reddit.com/"
         self._oauthurl = "https://oauth.reddit.com/"
-
-        self.token = None
-        self.headers = None
-        self.subreddit = None
-
-    def sub_search(self, name, limit, timeframe):
-        """Looks for subreddit matching name and creates Subreddit
-        class with corresponding details.
-        """
-        self.set_headers()
-        search_url = self._oauthurl + "search"
-        payload = {"q" : name,
-                   "t" : timeframe,
-                   "limit" : limit,
-                   "type" : "sr"}
-        result = requests.get(search_url, params=payload,
-                              headers=self.headers)
-        data = handle_response(result)
-        print(data)
+        self._token = None
+        self._headers = None
 
     def auth(self):
         """Authenticate with reddit API. Sets a variable that
@@ -74,24 +60,49 @@ class Reddit:
                                  auth=auth)
         data = handle_response(response)
         if data:
-            self.token = data["access_token"]
+            self._token = data["access_token"]
         else:
             raise Exception("bad data")
 
     def set_headers(self):
         """Sets default headers for OAuth requests."""
-        self.headers = {"Authorization" : "bearer " + self.token,
+        self._headers = {"Authorization" : "bearer " + self._token,
                         "User-Agent" : self._user_agent}
+
+    def get_subreddit(self, name):
+        """Creates Subreddit class with name and url if it exists. If
+        not, handle the 404 error.
+        """
+        self.set_headers()
+        sub_url = self._oauthurl + "r/" + name
+        try:
+            requests.get(sub_url, headers=self._headers)
+        except requests.exceptions.HTTPError as err:
+            if err.response.status_code == 404:
+                print("subreddit not found")
+        return Subreddit(name, sub_url)
+
+    def get_subreddit_posts(self, subreddit, sort="new", limit="10",
+                            timeframe="hour"):
+        """Searches for and updates the posts in a Subreddit
+        object. Specified options to edit search parameters.
+        """
+
 
 class Subreddit:
     """A class to represent a subreddit. It is a container for
     RedditPost objects.
     """
-
-    def __init__(self, name, id_):
+    def __init__(self, name, url):
         """Initialize Subreddit object."""
-        self.type_prefix = "t5_"
-        self.id_ = id_
-        # from the reddit API, fullnames are the combination of its
-        # type and unique ID, e.g. t3_15bfi0
-        self.fullname = self.type_prefix + self.id_
+        self.name = name
+        self.url = url
+        self.posts = []
+
+
+class RedditPost:
+    """A class to represent a reddit post. It keeps track of title, id
+    post content, and images.
+    """
+    def __init__(self, title, content, url):
+        """Initialize using id."""
