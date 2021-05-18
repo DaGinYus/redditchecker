2440b4f4d7357c046f56a43042d9d3f02ddc75d0
Matthew Wong <matthew@adveca.com>
2021-05-17 21:37:28 -0700

    changed default logging level

--- bot.py
+++ bot.py
@@ -157,7 +157,7 @@ class DiscordClient(discord.Client):
 def main():
     """Reads config and starts the bot."""
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s %(levelname)-8s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[logging.FileHandler("debug.log", 'w'),
